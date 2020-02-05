ad524c77e92134174817ba0f5e9d5c9db99ddc05
Francisco Suárez Mulero <francisco.suarez@sepe.es>
2020-02-05 12:31:15 +0100

    Features defined

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -18,5 +19,69 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given There are current prices in the system
+     */
+    public function thereAreCurrentPricesInTheSystem()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have a file named :arg1 with the new prices
+     */
+    public function iHaveAFileNamedWithTheNewPrices($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I upload the file
+     */
+    public function iUploadTheFile()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then Changes are applied to the current prices
+     */
+    public function changesAreAppliedToTheCurrentPrices()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I have a file named :arg1 with invalid data
+     */
+    public function iHaveAFileNamedWithInvalidData($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then A message is shown explaining the problem
+     */
+    public function aMessageIsShownExplainingTheProblem()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then Changes are not applied to the current prices
+     */
+    public function changesAreNotAppliedToTheCurrentPrices()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When There is an error in the system
+     */
+    public function thereIsAnErrorInTheSystem()
+    {
+        throw new PendingException();
     }
 }
